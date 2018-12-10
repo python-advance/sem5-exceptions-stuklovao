cf14a78e8ee10d6012b58e57058c496379f3a65e
stuklovao <36235933+stuklovao@users.noreply.github.com>
2018-12-10 14:59:38 +0300

    Update main.py

--- INVAR/main.py
+++ INVAR/main.py
@@ -1,1 +1,29 @@
+import json
+from prettytable import PrettyTable
 
+Book ='Book.json'
+
+def read_file(Book):
+  try:
+        file_book = open(Book, 'r') 
+        data = json.loads(file_book.read())
+  except FileNotFoundError:
+        print ("Ошибка! Файл не найден!")
+
+  with open(Book, 'r') as f:
+     data = json.loads(f.read())
+
+  name = []
+  surname = []
+  for i in range(len(data["Partisipants"])):
+    name.append(data["Partisipants"][i]["Name"])
+    surname.append(data["Partisipants"][i]["Surname"])
+  
+  x = PrettyTable()
+  column_names = ['Name', 'Surname']
+  x.add_column(column_names[0], name)
+  x.add_column(column_names[1], surname)
+  print(x)
+  return len(data["Partisipants"]) 
+
+read_file('Book.json')
